b0addab326d436662c26d74696eb3cfc26ea3676
abelabdia <abelabdia@gmail.com>
2025-06-05 01:42:32 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-RECEIVER_IP = "192.168.11.156"  # Replace with the IP of the other Raspberry Pi
+RECEIVER_IP = "192.168.137.239"  # Replace with the IP of the other Raspberry Pi
 PORT = 5005
 STARTER_BIT = np.array([1, 0, 1, 0, 1, 0, 1, 0], dtype=np.uint8)
+AUTH_PASSWORD = "raspberry_secure_chat"
+
+
